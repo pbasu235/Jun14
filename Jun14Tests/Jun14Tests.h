f5cd19232dc30fea86505f6dd05602fb43d3e865
pbasu235 <pbasu@concentricpharma.com>
2012-06-14 16:16:40 -0400

    Initial Commit

---
+++ Jun14Tests/Jun14Tests.h
@@ -0,0 +1,13 @@
+//
+//  Jun14Tests.h
+//  Jun14Tests
+//
+//  Created by Piyal Basu on 6/14/12.
+//  Copyright (c) 2012 Concentric. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface Jun14Tests : SenTestCase
+
+@end
---
+++ Jun14Tests/Jun14Tests.m
@@ -0,0 +1,32 @@
+//
+//  Jun14Tests.m
+//  Jun14Tests
+//
+//  Created by Piyal Basu on 6/14/12.
+//  Copyright (c) 2012 Concentric. All rights reserved.
+//
+
+#import "Jun14Tests.h"
+
+@implementation Jun14Tests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in Jun14Tests");
+}
+
+@end
